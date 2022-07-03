5f8a0897b747e552e68b4e85118c39660d29a4d3
Suravi <suravipk@gmail.com>
2022-07-03 10:56:55 +0530

    remove matrix

--- btmanager2.py
+++ btmanager2.py
@@ -5,12 +5,6 @@ import datetime
 import sys
 import time
 
-import click
-
-from time import gmtime, strftime
-
-from prometheus_client import Gauge, start_http_server, Enum
-
 global data
 #data = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
 data = {}
@@ -98,9 +92,19 @@ class AnyDevice(gatt.Device):
                 data['V06'] = self.rawdat['V06']
                 data['V07'] = self.rawdat['V07']
                 data['V08'] = self.rawdat['V08']
+                data['V09'] = self.rawdat['V09']
+                data['V10'] = self.rawdat['V10']
+                data['V11'] = self.rawdat['V11']
+                data['V12'] = self.rawdat['V12']
+                data['V13'] = self.rawdat['V13']
+                data['V14'] = self.rawdat['V14']
+                data['V15'] = self.rawdat['V15']
+                data['V16'] = self.rawdat['V16']
                 data['Bal'] = self.rawdat['Bal']
                 data['State'] = self.rawdat['State']
                 data['FET_St'] = self.rawdat['FET_St']
+
+                print(data)
 
                 self.manager.stop()
             else:
@@ -127,103 +131,6 @@ class AnyDevice(gatt.Device):
 
 
 
-class Metrics():
-
-    def __init__(self):
-        self.last_update_gauge = Gauge('bms_last_update', 'Metrics of last update')
-        self.ah_percent_gauge = Gauge('bms_ah_percent', 'Metrics of percent of capacity')
-        self.ah_remaining_gauge = Gauge('bms_ah_remaining', 'Metrics of remaining capcity')
-        self.ah_full_gauge = Gauge('bms_ah_full', 'Metrics of full capacity')
-        self.power_gauge = Gauge('bms_power', 'Metrics of power')
-        self.voltage_gauge = Gauge('bms_voltage', 'Metrics of voltage')
-        self.current_gauge = Gauge('bms_current', 'Metrics of current')
-        self.temperature_gauge = Gauge('bms_temperature', 'Metrics of temperature')
-        self.cycles_gauge = Gauge('bms_cycles', 'Metrics of cycles')
-        self.mode_enum = Enum('bms_mode', 'Metrics of mode', states=['discharging', 'charging'])
-        self.time_left = Gauge('bms_time_left', 'Metrics of time to charge/discharge', ['mode'])
-        self.temperature2_gauge = Gauge('bms_temperature2', 'Metrics of temperature2')
-        self.voltage01_gauge = Gauge('bms_voltage01', 'Metrics of voltage01')
-        self.voltage02_gauge = Gauge('bms_voltage02', 'Metrics of voltage02')
-        self.voltage03_gauge = Gauge('bms_voltage03', 'Metrics of voltage03')
-        self.voltage04_gauge = Gauge('bms_voltage04', 'Metrics of voltage04')
-        self.voltage05_gauge = Gauge('bms_voltage05', 'Metrics of voltage05')
-        self.voltage06_gauge = Gauge('bms_voltage06', 'Metrics of voltage06')
-        self.voltage07_gauge = Gauge('bms_voltage07', 'Metrics of voltage07')
-        self.voltage08_gauge = Gauge('bms_voltage08', 'Metrics of voltage08')
-        #self.bal_gauge = Gauge('bms_bal', 'Metrics of bal')
-        self.bal_status = Gauge('bms_balance_status', 'Metrics of balance status', ['cell'])
-        self.bms_state = Gauge('bms_state', 'Metrics of bms state', ['state'])
-        self.bms_fetst = Gauge('bms_fetst', 'Metrics of bms fetst', ['fetst'])
-
-    def build_metrics(self):
-        last_update = datetime.datetime.now()
-        # last_update = datetime.datetime.strptime(last_update, '%Y-%m-%d %H:%M:%S')
-        last_update = last_update.timestamp()
-        self.last_update_gauge.set(last_update)
-        self.ah_percent_gauge.set(data['Ah_percent'])
-        ah_remaining_gauge = data['Ah_remaining']
-        self.ah_remaining_gauge.set(ah_remaining_gauge)
-        ah_full_gauge = data['Ah_full']
-        self.ah_full_gauge.set(ah_full_gauge)
-        self.power_gauge.set(data['P'])
-        self.voltage_gauge.set(data['Vbat'])
-        self.current_gauge.set(data['Ibat'])
-        self.temperature_gauge.set(data['T1'])
-        self.cycles_gauge.set(data['Cycles'])
-        self.temperature2_gauge.set(data['T2'])
-        self.voltage01_gauge.set(data['V01'])
-        self.voltage02_gauge.set(data['V02'])
-        self.voltage03_gauge.set(data['V03'])
-        self.voltage04_gauge.set(data['V04'])
-        self.voltage05_gauge.set(data['V05'])
-        self.voltage06_gauge.set(data['V06'])
-        self.voltage07_gauge.set(data['V07'])
-        self.voltage08_gauge.set(data['V08'])
-
-        celbals = [0b0,0b0,0b0,0b0,0b0,0b0,0b0,0b0]
-        for num in range(0,8):
-            celbals[num] = ( data['Bal']>>num ) & 0b1
-        self.bal_status.labels(cell='celbals[1]').set(celbals[0])
-        self.bal_status.labels(cell='celbals[2]').set(celbals[1])
-        self.bal_status.labels(cell='celbals[3]').set(celbals[2])
-        self.bal_status.labels(cell='celbals[4]').set(celbals[3])
-        self.bal_status.labels(cell='celbals[5]').set(celbals[4])
-        self.bal_status.labels(cell='celbals[6]').set(celbals[5])
-        self.bal_status.labels(cell='celbals[7]').set(celbals[6])
-        self.bal_status.labels(cell='celbals[8]').set(celbals[7])
-
-        bms_state = [0b0,0b0,0b0,0b0,0b0,0b0,0b0,0b0,0b0,0b0,0b0,0b0,0b0]
-        for num in range(0,13):
-            bms_state[num] = ( data['State']>>num ) & 0b1
-        self.bms_state.labels(state='Cell_Block_Over_Vol').set(bms_state[0])
-        self.bms_state.labels(state='Cell_Block_Under_Vol').set(bms_state[1])
-        self.bms_state.labels(state='Battery_Over_Vol').set(bms_state[2])
-        self.bms_state.labels(state='Battery_Under_Vol').set(bms_state[3])
-        self.bms_state.labels(state='Charging_Over_Temp').set(bms_state[4])
-        self.bms_state.labels(state='Charging_Under_Temp').set(bms_state[5])
-        self.bms_state.labels(state='Discharging_Over_Temp').set(bms_state[6])
-        self.bms_state.labels(state='Discharging_Under_Temp').set(bms_state[7])
-        self.bms_state.labels(state='Charging_Over_Current').set(bms_state[8])
-        self.bms_state.labels(state='Discharging_Over_Current').set(bms_state[9])
-        self.bms_state.labels(state='Short_Circuit').set(bms_state[10])
-        self.bms_state.labels(state='Fore-end_IC_Error').set(bms_state[11])
-        self.bms_state.labels(state='MOS_Software_Lock-in').set(bms_state[12])
-
-        fet_state = [0b0,0b0]
-        for num in range(0,2):
-            fet_state[num] = ( data['FET_St']>>num ) & 0b1
-        self.bms_fetst.labels(fetst='FET_charging').set(fet_state[0])
-        self.bms_fetst.labels(fetst='FET_discharging').set(fet_state[1])
-
-        if float(data['P']) >= 0:
-            mode = 'charging'
-        else:
-            mode = 'discharging'
-
-        self.mode_enum.state(mode)
-        print(self)
-
-
 if (len(sys.argv)<2):
     print("Usage: bms-shed.py <device_uuid>")
 else:
@@ -234,7 +141,4 @@ else:
         device.connect()
         print("main device.connect")
         manager.run()
-        print("main manager.run")
-        metrics.build_metrics()
-        print("main metrics.build_metrics")
-        time.sleep(10)
+        time.sleep(30)
